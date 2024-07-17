5d288cb35f4ffda077ba2766a597a395c44296dc
enterthestream <zaid9010@gmail.com>
2024-07-17 10:29:26 +0100

    adds /api/articles endpoint that responds with an array of article objects

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,15 @@
-const { selectArticleById } = require("../models");
+const { selectArticleById, selectArticles } = require("../models");
+
+exports.getArticles = (request, response, next) => {
+  selectArticles()
+    .then((articles) => {
+      response.status(200).send({ articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
 
 exports.getArticleById = (request, response, next) => {
   const { article_id } = request.params;
--- models/articles.model.js
+++ models/articles.model.js
@@ -1,8 +1,32 @@
 const db = require("../db/connection");
 
+function selectArticles() {
+  return db
+    .query(
+      `
+    SELECT
+      articles.title,
+      articles.author,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+      COUNT(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;
+    `
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+}
+
 function selectArticleById(articleId) {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .query("SELECT * FROM articles WHERE article_id = $1", [articleId])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({
@@ -14,4 +38,4 @@ function selectArticleById(articleId) {
     });
 }
 
-module.exports = { selectArticleById };
+module.exports = { selectArticleById, selectArticles };
